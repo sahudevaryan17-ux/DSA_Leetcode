4b86b3046ba7ca630fba5f5c84d99d80d266a2ae
Dev Aryan Sahu <sahudevaryan17@gmail.com>
2026-07-25 20:35:38 +0530

    Time: 0 ms (100%), Space: 15.1 MB (68.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,11 +6,12 @@ public:
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-
+//agar mid wla hi ans hai to usko return karo
             if (nums[mid] == target) return mid;
 
-            // left half is sorted
+            // left half is sorted// mid se chhota ya pehle 
             if (nums[start] <= nums[mid]) {
+//starting element chhota hai target se and // target mid se chhota hai// start <-----target----->mid
                 if (nums[start] <= target && target <= nums[mid]) {
                     end = mid - 1;      // target is in left half -> shrink towards it
                 } else {
@@ -19,6 +20,7 @@ public:
             }
             // right half is sorted
             else {
+                //mid <-----target----->end
                 if (nums[mid] <= target && target <= nums[end]) {
                     start = mid + 1;    // target is in right half -> shrink towards it
                 } else {
