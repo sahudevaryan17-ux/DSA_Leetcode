972d45a42dbb199f83a1787d72f6c0eb977d4a1f
Dev Aryan Sahu <sahudevaryan17@gmail.com>
2026-09-15 16:13:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (88.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<char> st;
+        for (auto c : s){
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            else{
+                if(st.empty()) return false;
+
+                if(c == ')' && st.top() != '('||
+                c == ']' && st.top() != '['||
+                c == '}' && st.top() != '{'){
+                        return false;
+                }
+
+                st.pop();
+                
+            }
+        }
+        return st.empty();
+    }
+};
