470d3d90533a8701585d2252d0a6caa147878a77
Dev Aryan Sahu <sahudevaryan17@gmail.com>
2026-09-16 17:10:25 +0530

    Time: 235 ms (5.02%), Space: 13.9 MB (56.79%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v;
+        for(int i = 0; i < n ; i++){
+            int count = 0;
+            for(int j = 0; j < n ; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                v.push_back(nums[i]);
+            }
+        }
+        return v;
+    }
+};
