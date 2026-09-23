ddef2a23ac68ebf299ebd9c7f431bc0eed288b8a
Dev Aryan Sahu <sahudevaryan17@gmail.com>
2026-09-23 20:24:33 +0530

    Time: 7 ms (55.07%), Space: 12.3 MB (85.27%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -6,20 +6,16 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-    //conditions - 1.koi bhi node ho sakta hai
-    //2. no head
-    //3.vo dle ho jaye ya na ho bus uski val nhi honi chahiye 
-    //node null na ho aue node ke age null na ho 
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* prev = NULL;
-        while(node != NULL && node -> next != NULL ){
+        ListNode* back = NULL;
+        while (node != NULL && node -> next != NULL){
             node -> val = node -> next -> val;
-            prev = node;
+            back = node;
             node = node -> next; 
         }
-        prev -> next = NULL;
-        delete(node);
+        back -> next = NULL;
+        delete(node); 
     }
 };
